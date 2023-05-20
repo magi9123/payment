bc913e8f10f228f6420899d4ab8935a7be97ed50
margaritaivanova <m.ivanova.esti@gmail.com>
2023-05-20 20:59:47 +0300

    change id to uuid in Marchant entity and add relation with Transaction entity,change in MerchantRepository

--- src/main/java/org/example/model/Merchant.java
+++ src/main/java/org/example/model/Merchant.java
@@ -1,31 +1,40 @@
 package org.example.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Merchant {
 
     @Id
-    private long id;
+    @GeneratedValue
+    private UUID id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column
     private String description;
 
+    @Column
     private String email;
 
+    @Column(nullable = false)
     private MerchantStatus status;
 
+    @Column
     private BigDecimal totalTransactionSum;
+
+    @OneToMany
+    @JoinColumn(name = "uuid")
+    private List<Transaction> transactionList;
 }
--- src/main/java/org/example/repositories/MerchantRepository.java
+++ src/main/java/org/example/repositories/MerchantRepository.java
@@ -4,6 +4,8 @@ import org.example.model.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface MerchantRepository extends JpaRepository<Merchant, Long> {
+public interface MerchantRepository extends JpaRepository<Merchant, UUID> {
 }
