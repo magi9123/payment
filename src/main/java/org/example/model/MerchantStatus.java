b9e4e594d914d35a764531c470ddfe2743cf0c22
margaritaivanova <m.ivanova.esti@gmail.com>
2023-05-26 08:19:45 +0300

    MerchantStatus remove private constructor

--- src/main/java/org/example/model/MerchantStatus.java
+++ src/main/java/org/example/model/MerchantStatus.java
@@ -1,10 +1,5 @@
 package org.example.model;
 
 public enum MerchantStatus {
-    ACTIV(1), INACTIVE(0);
-
-    private final int status;
-    MerchantStatus(int status) {
-        this.status = status;
-    }
+    INACTIVE, ACTIV
 }
